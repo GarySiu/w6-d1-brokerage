b6eb5d1bbcf7fa36cbe7ae7d96c12c90e8ef656e
Gary Siu <Gary@untypeable.com>
2015-09-16 08:11:07 +0100

    Defined class method for brokers to sell stock (with no validation whatsoever)

--- app/models/brokerage.rb
+++ app/models/brokerage.rb
@@ -1,4 +1,12 @@
 class Brokerage < ActiveRecord::Base
   has_many :holdings
   has_many :clients, through: :holdings
+
+  def sell(holding,quantity)
+    sale = self.holdings.find(holding)
+    sale.client.update(cash_balance: (sale.client.cash_balance += (sale.stock.price * quantity)))
+    sale.update(shares: (sale.shares -= quantity))
+    self.holdings.destroy sale if sale.shares == 0
+  end
+
 end
